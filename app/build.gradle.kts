888b2a3ac87659d9392c9e36ae01168d56ae4d39
toufikben <6.6898475e+07+toufikben@users.noreply.github.com>
2026-09-06 20:05:58 +0000

    Release Phone Fortress version 1.0.0 build 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     applicationId = "com.toufikbendjeddah.phonefortress"
     minSdk = 24
     targetSdk = 36
-    versionCode = 5
+    versionCode = 6
     versionName = "1.0.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
